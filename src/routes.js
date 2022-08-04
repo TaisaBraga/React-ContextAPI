4fd1ecdbb6555f755595fa6312c89eccbf1d0f04
Taisa Braga Bento <taisabraga.adm@gmail.com>
2022-08-04 16:48:02 -0300

    Add props to Login area

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,21 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "pages/Login";
 import Feira from "pages/Feira";
 import Carrinho from "pages/Carrinho";
+import { useState } from "react";
 
 function Router() {
+  const [nome, setNome] = useState("");
+  const [saldo, setSaldo] = useState(0);
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <Login />
+          <Login
+            nome={nome}
+            setNome={setNome}
+            saldo={saldo}
+            setSaldo={setSaldo}
+          />
         </Route>
         <Route path="/feira">
           <Feira />
